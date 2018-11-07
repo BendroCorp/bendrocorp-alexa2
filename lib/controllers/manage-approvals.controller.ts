a88e24001fd0716325c80979a680850c5426e5e3
Dale Myszewski II <dale.myszewski@seagate.com>
2018-11-06 22:07:38 -0600

    Fixed issues with manage approvals

--- lib/controllers/manage-approvals.controller.ts
+++ lib/controllers/manage-approvals.controller.ts
@@ -2,6 +2,7 @@ import { HandlerInput, RequestHandler } from 'ask-sdk-core';
 import { Response, IntentRequest, } from 'ask-sdk-model';
 import { ApiService } from '../services/api.service';
 import { Globals } from '../globals';
+import { resolve } from 'url';
 
 export const ManageApprovalsRequestHandler : RequestHandler = {
     canHandle(handlerInput : HandlerInput) : boolean {
@@ -9,6 +10,37 @@ export const ManageApprovalsRequestHandler : RequestHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'ManageApprovalsIntent'
     },
     handle(handlerInput : HandlerInput) : Promise<Response> {
+      // check to make sure that everything is done
+      let request = (handlerInput.requestEnvelope.request as IntentRequest)
+
+      let parseApprovalActionSlot = (slotValue:string) => {
+        if (slotValue) {
+          if (slotValue.toLowerCase() === "approve") {
+            return 4;
+          }else if (slotValue.toLowerCase() === "decline") {
+            return 5;
+          }else {
+            return -1;
+          }
+        }
+      
+        return -1;
+      }      
+
+      if (request.dialogState == 'STARTED') {
+        return new Promise((resolve, reject) => {
+          resolve(handlerInput.responseBuilder.addDelegateDirective(request.intent).getResponse())
+        })
+      }else if (request.dialogState != 'COMPLETED')
+      {
+        return new Promise((resolve, reject) => {
+          resolve(handlerInput.responseBuilder.addDelegateDirective().getResponse())
+        })
+      }else{
+
+      }
+
+      // if everything is done then we should be done
       const accessToken = handlerInput.requestEnvelope.session.user.accessToken
       let api:ApiService = new ApiService(new Globals)
       
@@ -17,12 +49,19 @@ export const ManageApprovalsRequestHandler : RequestHandler = {
           api.approvals(accessToken).then((response) => {
             const responseData = response.data
             if (responseData.filter(x => x.approval_type_id < 4).length > 0) {
+              
+              // we need to make sure our slots are filled
+              // delegateSlotHandler(this)
+              (handlerInput.requestEnvelope.request as IntentRequest).intent.confirmationStatus
+
               let approvalActionSlotValue = (handlerInput.requestEnvelope.request as IntentRequest).intent.slots.approvalAction.value //handlerInput.requestEnvelope.request.intent.slots.approvalAction.value
               let approvalIdSlotValue = (handlerInput.requestEnvelope.request as IntentRequest).intent.slots.approvalId.value //handlerInput.requestEnvelope.request.intent.slots.approvalId.value
-  
+              console.log("we got past the slot picks up");
+              console.log((handlerInput.requestEnvelope.request as IntentRequest).intent.slots.approvalAction);
+              console.log((handlerInput.requestEnvelope.request as IntentRequest).intent.slots.approvalId);
+
               if (responseData.filter(x => x.approval_type_id < 4).find(x => x.approval_id == parseInt(approvalIdSlotValue))) {
-                let approval_type_id = this.parseApprovalActionSlot(approvalActionSlotValue)
-  
+                let approval_type_id = parseApprovalActionSlot(approvalActionSlotValue)
                 if (approval_type_id != -1) {
                   api.submit_approval(accessToken, parseInt(approvalIdSlotValue), approval_type_id).then(
                     response => {
@@ -55,6 +94,8 @@ export const ManageApprovalsRequestHandler : RequestHandler = {
                 .getResponse() as Response)
             }
           }).catch((error) => {
+            console.log("Error Occured")            
+            console.log(error)            
             resolve(handlerInput.responseBuilder
               .speak("I received an error while attempting to fetch approvals data from BendroCorp. Please contact an executive member on Discord.")
               .getResponse() as Response);
